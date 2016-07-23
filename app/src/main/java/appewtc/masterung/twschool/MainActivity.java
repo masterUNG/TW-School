728f7b7e0ba757ad0248141cfdb4b4b9970eefd9
masterUNG <phrombutr@gmail.com>
2016-07-23 16:03:52 +0700

    Design UI SignUp Finish

--- app/src/main/java/appewtc/masterung/twschool/MainActivity.java
+++ app/src/main/java/appewtc/masterung/twschool/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.twschool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     //Get Event from Click
     public void clickSignUpMain(View view) {
+
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
 
     }
 
