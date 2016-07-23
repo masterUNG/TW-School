16c48618bc2ca89b597cf0455eceeb0738988fde
masterUNG <phrombutr@gmail.com>
2016-07-23 14:14:59 +0700

    Create Button Finish1

--- app/src/main/java/appewtc/masterung/twschool/MainActivity.java
+++ app/src/main/java/appewtc/masterung/twschool/MainActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.twschool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    //Get Event from Click
+    public void clickSignUpMain(View view) {
+
     }
-}
+
+}   // Main Class นี่คือคลาสหลัก
