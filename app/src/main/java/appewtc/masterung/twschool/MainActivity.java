a3e53bc963040ee0adac818858f4e0437630c598
masterUNG <phrombutr@gmail.com>
2016-07-24 17:03:10 +0700

    Finish24July

--- app/src/main/java/appewtc/masterung/twschool/MainActivity.java
+++ app/src/main/java/appewtc/masterung/twschool/MainActivity.java
@@ -109,6 +109,23 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(context, "Welcome " + loginStrings[1] + " " + loginStrings[2],
                             Toast.LENGTH_SHORT).show();
 
+                    //Intent to Service
+                    switch (Integer.parseInt(loginStrings[3])) {
+                        case 0:
+                            //Teacher
+                            break;
+                        case 1:
+                            //Student
+
+                            Intent intent = new Intent(MainActivity.this, StudentService.class);
+                            intent.putExtra("Login", loginStrings);
+                            startActivity(intent);
+                            finish();
+
+                            break;
+                    } //switch
+
+
                 } else {
                     //Password False
                     Toast.makeText(context, "Please Try Again Password False",
