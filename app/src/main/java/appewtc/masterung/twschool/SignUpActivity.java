b2419aabbfdf01118a0aff55a399402813848487
masterUNG <phrombutr@gmail.com>
2016-07-24 10:35:03 +0700

    Confirm Dialog finish

--- app/src/main/java/appewtc/masterung/twschool/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/twschool/SignUpActivity.java
@@ -1,5 +1,7 @@
 package appewtc.masterung.twschool;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,7 +18,8 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton studentRadioButton, teacherRadioButton;
     private String nameString, surnameString, roomString, userString,
-            passwordString, statusString, QRcodeString;
+            passwordString, statusString = "1", QRcodeString;
+
 
 
     @Override
@@ -33,6 +36,29 @@ public class SignUpActivity extends AppCompatActivity {
         radioGroup = (RadioGroup) findViewById(R.id.ragStatus);
         studentRadioButton = (RadioButton) findViewById(R.id.radioButton);
         teacherRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+
+                    case R.id.radioButton:
+                        //for Student
+                        statusString = "1";
+
+                        break;
+                    case R.id.radioButton2:
+                        //for Teacher
+                        statusString = "0";
+
+                        break;
+
+                }   // switch
+
+            }   // onCheck
+        });
 
 
     }   // Main Method
@@ -56,10 +82,73 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            switch (Integer.parseInt(statusString)) {
+
+                case 0:
+                    QRcodeString = "teacher_" + userString;
+                    break;
+                case 1:
+                    QRcodeString = "student_" + userString;
+                    break;
+
+            }   // switch
+
+            confirmValue();
 
         }   // if
 
 
     }   // clickSignUp
+
+    private void confirmValue() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("โปรดตรวจสอบข้อมูล");
+        builder.setMessage("Name = " + nameString + "\n" +
+        "Surname = " + surnameString + "\n" +
+        "Status = " + showStatus(statusString) + "\n" +
+        "Room = " + roomString + "\n" +
+        "User = " + userString + "\n" +
+        "Password = " + passwordString + "\n" +
+        "QRcode = " + QRcodeString);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                upLoadValueToServer();
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }   // confirmValue
+
+    private void upLoadValueToServer() {
+
+    }   // upLoad
+
+    private String showStatus(String statusString) {
+
+        String result = null;
+
+        switch (Integer.parseInt(statusString)) {
+            case 0:
+                result = "Teacher";
+                break;
+            case 1:
+                result = "Student";
+                break;
+        }
+
+        return result;
+    }
 
 }   // Main Class
