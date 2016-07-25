b9a56741d574e92bc7637c508f25dc8c8ab0c3c8
masterUNG <phrombutr@gmail.com>
2016-07-25 13:36:18 +0700

    Check Point Finish

--- app/src/main/java/appewtc/masterung/twschool/EditStudent.java
+++ app/src/main/java/appewtc/masterung/twschool/EditStudent.java
@@ -1,13 +1,16 @@
 package appewtc.masterung.twschool;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -17,6 +20,7 @@ public class EditStudent extends FragmentActivity implements OnMapReadyCallback 
     private GoogleMap mMap;
     private EditText nameEditText, surnameEditText, roomEditText;
     private String[] loginStrings;
+    private double studentLatADouble = 0, studentLngADouble = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +44,71 @@ public class EditStudent extends FragmentActivity implements OnMapReadyCallback 
         mapFragment.getMapAsync(this);
     }   // Main Method
 
+    public void clickEditData(View view) {
+
+        if (studentLatADouble != 0) {
+
+            uploadValue();
+
+        } else {
+
+            Toast.makeText(this, "กรุณาคลิกที่แผนที่ เพื่อกำหนดตำแหน่งของบ้าน",
+                    Toast.LENGTH_SHORT).show();
+
+        }   // if
+
+
+    }   // clickEdit
+
+    private void uploadValue() {
+
+
+
+    }   // upload
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //For Setup Center Map
+        final double twLat = 15.350664;   // Latitude ของ ตากฟ้า
+        final double twLng = 100.491939;
+
+        LatLng latLng = new LatLng(twLat, twLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        createTWmarker(latLng);
+
+        //Find Student Home
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                mMap.clear();
+                createTWmarker(new LatLng(twLat, twLng));
+
+                mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(loginStrings[1])
+                .snippet(Double.toString(latLng.latitude) + ", " +
+                Double.toString(latLng.longitude)));
+
+                studentLatADouble = latLng.latitude;
+                studentLngADouble = latLng.longitude;
+
+
+            }   // onMapClick
+        });
+
+
     }   // onMap
+
+    private void createTWmarker(LatLng latLng) {
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.build4))
+                .title("โรงเรียนตากฟ้าวิชาประสิทธิ์"));
+    }
 
 }   // Main Class
