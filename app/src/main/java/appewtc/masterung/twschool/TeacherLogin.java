4332d96250c7dd8113065515932a7d51ab37e551
masterUNG <phrombutr@gmail.com>
2016-09-12 15:15:22 +0700

    Query Where Room and Status finish

--- app/src/main/java/appewtc/masterung/twschool/TeacherLogin.java
+++ app/src/main/java/appewtc/masterung/twschool/TeacherLogin.java
@@ -1,15 +1,26 @@
 package appewtc.masterung.twschool;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class TeacherLogin extends FragmentActivity implements OnMapReadyCallback {
 
@@ -17,6 +28,8 @@ public class TeacherLogin extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private String[] userLoginStrings;
     private TextView textView;
+    private static final String urlJSON = "http://swiftcodingthai.com/tw/get_user_where_room_master.php";
+    private String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +54,45 @@ public class TeacherLogin extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //For Setup Center Map
+        final double twLat = 15.350664;   // Latitude ของ ตากฟ้า
+        final double twLng = 100.491939;
+
+        LatLng latLng = new LatLng(twLat, twLng);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        //For Marker School
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.tw_logo48))
+                .title("โรงเรียนตากฟ้าวิชาประสิทธิ์")
+                .snippet("โรงเรียนมัธยมอันดับหนึ่งของ นครสวรรค์"));
+
+        //Create Student Marker
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Room", userLoginStrings[4])
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlJSON).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                jsonString = response.body().string();
+                Log.d("12SepV2", "JSON ==> " + jsonString);
+
+
+            }   // onResponse
+        });
+
+
     }   // onMapReady
 
 }   // Main Class
