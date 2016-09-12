a4f0f68b6a49b1e70679e0026edd40e3fc30949a
masterUNG <phrombutr@gmail.com>
2016-09-12 14:23:08 +0700

    Show View Finish

--- app/src/main/java/appewtc/masterung/twschool/TeacherLogin.java
+++ app/src/main/java/appewtc/masterung/twschool/TeacherLogin.java
@@ -2,6 +2,7 @@ package appewtc.masterung.twschool;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,28 +13,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class TeacherLogin extends FragmentActivity implements OnMapReadyCallback {
 
+    //Explicit
     private GoogleMap mMap;
+    private String[] userLoginStrings;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_login_layout);
+
+        textView = (TextView) findViewById(R.id.textView15);
+
+        userLoginStrings = getIntent().getStringArrayExtra("Login");
+
+        textView.setText(userLoginStrings[1] + " " +
+        userLoginStrings[2] + " ห้อง " + userLoginStrings[4]);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    }   // Main Method
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +45,6 @@ public class TeacherLogin extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }   // onMapReady
+
+}   // Main Class
