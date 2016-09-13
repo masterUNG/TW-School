ce2eaa649056bd0989d44e85e7cc0774e3229320
masterUNG <phrombutr@gmail.com>
2016-09-13 10:25:30 +0700

    Design UI ListView finish

---
+++ app/src/main/java/appewtc/masterung/twschool/ListAllStudent.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.twschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListAllStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_all_student);
+    }
+}
